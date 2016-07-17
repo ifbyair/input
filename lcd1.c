933bffb99604052e3471900c3d877078b194e3a8
Max Chebb <minimax@gmail.com>
2016-07-17 12:41:33 -0700

    LCD real work began

---
+++ lcd1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#include <wiringPi.h>
+#include <lcd.h>
+
+#ifndef TRUE
+#  define   TRUE    (1==1)
+#  define   FALSE   (1==2)
+#endif
+
+static int lcdHandle;
+
+int main(int ac, char *av[]){
+    wiringPiSetup();
+
+    lcdHandle = lcdInit(4,20,4,7,17,4,5,6,7,0,0,0,0);
+
+    if( lcdHandle < 0 ){
+        printf("Problem with lcdInit\n");
+        exit(0);
+    }
+
+    return 1;
+}
