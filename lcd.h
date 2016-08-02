5f0f6a17f8f8016d8f96ef91f64acdc640509513
Max Chebb <minimax@gmail.com>
2016-08-01 21:00:42 -0700

    2016/08/01

---
+++ lcd.h
@@ -0,0 +1,13 @@
+/*
+Defining values for 20X4 4-bits LCD
+*/
+
+#define LCD_ROWS    4   # rows
+#define LCD_COLS    20  # columns
+#define LCD_BITS    4
+#define LCD_ENBL    10  # enable GPIO
+#define LCD_CLK     11  # clock GPIO
+#define LCD_BIT0    4
+#define LCD_BIT1    5
+#define LCD_BIT2    6
+#define LCD_BIT3    7
